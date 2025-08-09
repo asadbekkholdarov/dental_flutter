5a052cf8d7a84a5ef46d9d8023d6b9e5fcd018dd
asadbekkholdarov <507asadali@gmail.com>
2025-08-09 13:51:33 +0500

    admin login v1

--- start.sh
+++ start.sh
@@ -1,2 +1,10 @@
+#!/bin/bash
+set -e
+
 chmod +x pocketbase/pocketbase
+
+# Avtomatik super admin yaratish (agar mavjud bo'lmasa)
+./pocketbase/pocketbase admin create admin@gmail.com admin12345 || true
+
+# PocketBase serverini ishga tushirish
 ./pocketbase/pocketbase serve --http 0.0.0.0:10000
